ca99d88c148dbff87d49c395cc4e2f2e1b14f1aa
axel <axel@pop-os.localdomain>
2021-11-19 10:22:28 +0100

    basic html structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>Learn React @Becode</h1>
+      <header>
+        <h1>My Todo App</h1>
+      </header>
+
+      <main>
+        <div>
+          <input type="text" value="" placeholder="Type a new todo"/>
+          <button>Add Todo</button>
+        </div>
+
+        <div>
+          <h2>Todos</h2>
+          <ul>
+            <li>
+              <input type="checkbox" id="learn-react" />
+              <label htmlFor="learn-react">Learn React</label>
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
