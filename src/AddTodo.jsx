3b454010ea85aff669bc4f80e2bd50604027d45a
axel <axel@pop-os.localdomain>
2021-11-19 11:44:20 +0100

    retrieving the value of input using useRef hook and a clickHandler function

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useRef }from 'react';
 
 const AddTodo = () => {
+    const inputRef = useRef()
+
+    const clickHandler = () => {
+        const inputElement = inputRef.current
+        const inputValue = inputElement.value
+        console.log(inputValue)
+    }
+
     return ( 
         <div>
-          <input type="text" value="" placeholder="Type a new todo"/>
+          <input ref={inputRef} type="text" placeholder="Type a new todo"/>
 
-          <button>Add Todo</button>
+          <button onClick={clickHandler}>Add Todo</button>
         </div>
      );
 }
